9d00f18dbb3d531ff95f00430b7f065bcc531bfe
nikondra <96941514+nikondra@users.noreply.github.com>
2022-01-10 03:36:23 +0300

    add initial conditines passing each level

--- training/rules/levels.js
+++ training/rules/levels.js
@@ -1,1 +1,13 @@
+let rules = {"1": `<div>in this level we train the keyboard keys. there will be a hint</div>
+        <div>to pass the level, need: <=10% mistakes, >50 characters per minute</div>`,
+    "2": `<div>in this level we train the keyboard keys</div>
+        <div>to pass the level, need: <=10% mistakes, >80 characters per minute</div>`,
+    "3": `<div>in this level we train confident (+upper case) typing on the keyboard</div>
+        <div>to pass the level, need: <=7% mistakes, >120 characters per minute</div>`,
+    "4": `<div>in this level we train the keyboard keys under the metronome</div>
+        <div>to pass the level, need: <=5% mistakes, >150 characters per minute</div>`,
+    "5": `<div>in this level we train commands of VIM</div>
+        <div>to pass the level, need: <=3% mistakes, >200 characters per minute</div>`
+}
 
+let rulesStrong = [[10, 50], [10, 80], [7, 120], [5, 150], [3, 200]]
