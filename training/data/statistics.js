c6f9ac046e55a99c0616285742ff783edcbb742d
nikondra <96941514+nikondra@users.noreply.github.com>
2022-01-18 01:24:23 +0300

    Create statistics.js

---
+++ training/data/statistics.js
@@ -0,0 +1,12 @@
+let statistics = {
+    difficultSymbols: [],
+    //byLevels: [],
+    //accuracy: 0,
+    speed: 0
+}
+
+let unprocessedStatistic = []
+// for all typing
+let tempStatisticForSpeed = []
+// typing on blocks (because might be delay)
+let tempBlocksStatisticForSpeed = []
