9005cb8cd38e17eb9e90b641be1d01f927cd1dca
nikondra <96941514+nikondra@users.noreply.github.com>
2022-01-22 05:24:48 +0300

    Update after-level.js

--- training/visual/after-level.js
+++ training/visual/after-level.js
@@ -1,1 +1,13 @@
+let afterLevelAvailableCommands = {
+    "any button": ["continue train", "update+render state.visual=level"],
+    "Enter": ["finish the train", "update+render state.visual=initial-level"]
+}
 
+let afterLevelComponents = {
+    "stat": ``,
+    "state": ``,
+    "current": '',
+    "info": `training on pause`,
+    "available": `available commands: ${myObjToStr(afterLevelAvailableCommands)}`,
+    "modal": ``
+}
