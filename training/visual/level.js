432a23a60ae684a21c95e66e24cd9558f3946ff4
nikondra <96941514+nikondra@users.noreply.github.com>
2022-01-25 04:18:30 +0300

    Update level.js

--- training/visual/level.js
+++ training/visual/level.js
@@ -1,1 +1,13 @@
+let levelAvailableCommands = {
+    "any button": ["press the correct one", "train key="],
+    "Enter": ["pause train", "update+render state.visual=after-level"]
+}
 
+let levelComponents = {
+    "stat": [`stat: <not-error>0</not-error>/<error>0</error> (<percentage></percentage>)`],
+    "state": ``,
+    "current": `${symbols[0]}`,
+    "info": '',
+    "available": `available commands: ${myObjToStr(levelAvailableCommands)}`,
+    "modal": ``
+}
