521d5386d4b24f636e54b60640b6fdcbe781d1d3
nikondra <96941514+nikondra@users.noreply.github.com>
2022-01-25 04:19:04 +0300

    Update pre-level.js

--- training/visual/pre-level.js
+++ training/visual/pre-level.js
@@ -1,1 +1,12 @@
+let preLevelAvailableCommands = {
+    "any button": ["start train", "update+render state.visual=level"]
+}
 
+let preLevelComponents = {
+    "stat": `welcome to exercises for VIM`,
+    "state": ``,
+    "current": `${state.level} level of training VIM`,
+    "info": [`rules: ${rules[''+state.level+'']}`, `<next>type <kb>any button</kb></next>`],
+    "available": `available commands: ${myObjToStr(preLevelAvailableCommands)}`,
+    "modal": ``
+}
